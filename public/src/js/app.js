8afb7eb1d85eb2ce842692879da5417679f25427
siddharthsharma96 <sid9nov@gmail.com>
2020-08-21 10:40:03 +0530

    Progressive web app fallback page

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,15 +1,24 @@
+
 var deferredPrompt;
 
-if('serviceWorker' in navigator){
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then(function(){
-            console.log('service worker registered')
-        })
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-window.addEventListener('beforeinstallprompt',function(event){
-    console.log('beforeinstall prompt fired');
-    event.preventDefault();
-    deferredPrompt=event;
-})
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
